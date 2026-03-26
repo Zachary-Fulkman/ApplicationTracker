e92985daeabc2fc52285ed023e594270d2d65c35
Zachary-Fulkman <163482286+Zachary-Fulkman@users.noreply.github.com>
2026-03-26 16:49:41 -0400

    Fixing backend breaking upon deployment due to migration

--- Program.cs
+++ Program.cs
@@ -28,8 +28,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
+app.UseSwagger();
     app.UseSwaggerUI();
 
 
